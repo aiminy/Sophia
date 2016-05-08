ffc0f2112438b7d144b7575229684e1041aeba7a
aiminy <jlfmssm@gmail.com>
2016-05-08 13:17:18 -0400

    Move generatePCA and heatmap_wPCA to seperate functions

--- R/all_R_code.R
+++ R/all_R_code.R
@@ -5565,71 +5565,9 @@ object <- frma(AffyBatchExample)
 #' @export
 #'
 #' @examples
-GeneratePCA<-function(reorder.column.ed.set123.frma.cancer,GeneSym.all,data.set123.raw.with.set.label.and.type.cancer,output_file,output_dir){
 
-ndata<-reorder.column.ed.set123.frma.cancer
 
-geneSymbol=GeneSym.all
-tempdata.byGSym =data.frame(ndata, Symbol = geneSymbol)
-
-colnames(tempdata.byGSym)
-
-tempdata.byGSym.2<-tempdata.byGSym[-which(is.na(tempdata.byGSym$Symbol)),]
-
-data.byGSym = ddply(tempdata.byGSym.2, c("Symbol"),function(h)
-  summary = apply(h[,1:(dim(tempdata.byGSym.2)[2]-1)],2,max)
-)
-
-rownames(data.byGSym)=data.byGSym$Symbol
-colnames(data.byGSym)
-
-data.byGSym.2<-data.byGSym[,-1]
-temp = apply(data.byGSym.2,2,as.numeric)
-rownames(temp) = rownames(data.byGSym.2)
-colnames(temp) = colnames(ndata)
-
-data.byGSym.2 = temp
-colnames(data.byGSym.2)
-
-Draw_PCA(data.byGSym.2,output_file,output_dir,data.set123.raw.with.set.label.and.type.cancer[,2])
-
-}
-heatmap_wPCA = function(Data, output_heatmap, output_pca, out_dir, g_level = NULL){
-  hmcol<-rev(colorRampPalette(brewer.pal(10, "RdBu"))(256))
-  if(is.null(g_level)){
-    type_level = 1:ncol(Data)
-    col_level = "black"
-  }else{
-    type_level = 16
-    TEMP = factor(g_level)
-    uniq_label =  levels(TEMP)
-    levels(TEMP) = hmcol[ceiling(seq(length.out=length(levels(TEMP)),from=1,to=256))]
-    col_level = as.character(TEMP)
-    uniq_col = levels(TEMP)
-  }
-
-  Data.hc = hclust(dist(Data), method="average")
-  rowInd <- order.dendrogram(as.dendrogram(Data.hc))
-  pdf(file=paste0(out_dir,"/",output_heatmap), width=10, height=12)
-  heatmap.2(Data[rowInd,], col=hmcol, Rowv = F, dendrogram = "column", scale="row",labRow = NA,key=TRUE, keysize=0.55, symkey=FALSE,density.info="none", trace="none",cexCol=1,margins=c(8,12))
-  dev.off()
-
-  pdf(file=paste0(out_dir,"/",output_pca), width=12, height=12)
-  Data.pca = prcomp(t(Data))
-  with(data.frame(Data.pca$x), scatter3D(PC1, PC2, PC3, colvar = NULL, type="h",
-                                         ticktype = "detailed", bty="b2", cex=1,
-                                         xlab="PC 1",	ylab="PC 2",zlab="PC 3", theta = 40, phi = 40, pch=type_level,
-                                         col=col_level,
-                                         main = "Principal component analysis")
-  )
-  legend("topright", legend = uniq_label, pch=type_level,
-         col = uniq_col,
-         cex=1, inset=c(0.02))
-  with(data.frame(Data.pca$x), text3D(x=PC1, y=PC2,
-                                      z=PC3, colnames(Data), col = "black", add=TRUE, colkey = FALSE, cex=0.5)
-  )
-  dev.off()
-}load("/media/H_driver/Aimin_project/Sophia/Data_4_heatmap_PCA.RData")
+load("/media/H_driver/Aimin_project/Sophia/Data_4_heatmap_PCA.RData")
 library("plot3D")
 library("gplots")
 library(ggdendro)
